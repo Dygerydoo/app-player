0784d4879694addf588d59d88170ff7a17f4af88
Dygerydoo <caserofrancisco@gmail.com>
2017-06-20 14:41:23 +0200

    remove react boilerplate

--- src/routes/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { Route } from 'react-router';
-
-const routes = () =>
-  <Route path="/" component={() => <div>Start from here!</div>} />
-
-export default routes;
