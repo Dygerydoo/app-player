d2677f24aeb7335852d68f640fe04f5868622591
Dygerydoo <caserofrancisco@gmail.com>
2017-06-22 13:49:15 +0200

    create controls for player component

--- src/components/PlayerControls.vue
+++ src/components/PlayerControls.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="app-PlayerControls">
     <button class="app-PlayerControls_Reverse"><i class="material-icons">skip_previous</i></button>
-    <button v-if="isPlaying"class="app-PlayerControls_Play" @click="pauseMedia()"><i class="material-icons">pause</i></button>
+    <button v-if="$store.state.isPlaying"class="app-PlayerControls_Play" @click="pauseMedia()"><i class="material-icons">pause</i></button>
     <button v-else class="app-PlayerControls_Play" @click="playMedia()"><i class="material-icons">play_arrow</i></button>
     <button class="app-PlayerControls_Forward"><i class="material-icons">skip_next</i></button>
   </div>
@@ -10,21 +10,16 @@
 <script>
 export default {
   name: '',
-  data() {
-    return {
-      isPlaying: false,
-      isPaused: false,
-    };
-  },
   methods: {
     playMedia() {
-      this.isPaused = false;
+      this.$store.state.isPaused = false;
       this.$store.state.audio.play();
-      this.isPlaying = true;
+      this.$store.state.isPlaying = true;
     },
     pauseMedia() {
       this.$store.state.audio.pause();
-      this.isPlaying = false;
+      this.$store.state.isPlaying = false;
+      this.$store.state.isPaused = true;
     },
   },
 };
