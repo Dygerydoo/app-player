b424c7e1d3382721cad5497a6853ef1868e9a5a3
Dygerydoo <caserofrancisco@gmail.com>
2017-06-25 00:29:46 +0200

    give new styles for player buttons and add name to component

--- src/components/player/PlayerControls.vue
+++ src/components/player/PlayerControls.vue
@@ -1,15 +1,15 @@
 <template>
   <div class="app-PlayerControls">
-    <button class="app-PlayerControls_Previous" @click="prevTrack"><i class="material-icons">skip_previous</i></button>
+    <button class="app-Buttons_Primary-flatRounded" @click="prevTrack"><i class="material-icons">skip_previous</i></button>
     <button v-if="$store.state.isPlaying"class="app-Buttons_Primary-rounded" @click="pauseMedia()"><i class="material-icons">pause</i></button>
     <button v-else class="app-Buttons_Primary-rounded" @click="playMedia()"><i class="material-icons">play_arrow</i></button>
-    <button class="app-PlayerControls_Next" @click="nextTrack"><i class="material-icons">skip_next</i></button>
+    <button class="app-Buttons_Primary-flatRounded" @click="nextTrack"><i class="material-icons">skip_next</i></button>
   </div>
 </template>
 
 <script>
 export default {
-  name: '',
+  name: 'player-controls',
   methods: {
     playMedia() {
       this.$store.state.isPaused = false;
