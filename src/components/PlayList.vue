1461f58d14b766c8c26cd108fedf6ca060e89da8
Dygerydoo <caserofrancisco@gmail.com>
2017-06-24 16:12:23 +0200

    add clear current queue button

--- src/components/PlayList.vue
+++ src/components/PlayList.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="app-Queue">
+    <h1>Tu cola de reproducción</h1>
+    <button type="button" class="app-Button_Primary"@click="emptyQueue">Borrar cola</button>
     <song-list :list-content="$store.state.myQueue"
                @play-song="playSelected"></song-list>
   </div>
@@ -11,13 +13,16 @@ export default {
   name: 'app-queue',
   methods: {
     playSelected(event) {
-      const filteredSong = this.$store.state.searchResults.find(song => song.id === event);
+      const filteredSong = this.$store.state.myQueue.find(song => song.id === event);
       this.$store.state.selectedSong = filteredSong;
       this.$store.state.audio.addEventListener('loadeddata', () => {
         this.$store.state.audio.play();
         this.$store.state.isPlaying = true;
         this.searchQuery = '';
       });
+    },
+    emptyQueue() {
+      localStorage.removeItem('queue');
     },
   },
   components: {
