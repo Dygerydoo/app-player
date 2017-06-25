0711b71caea37c0ac89afa9e65a7c653508425e1
Dygerydoo <caserofrancisco@gmail.com>
2017-06-25 12:57:56 +0200

    add empty state to home component

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,14 @@
 <template>
   <div class="app-Home">
     <h1>Bienvenido</h1>
-    <p>Para crear una cola de reproducción, usa el buscador y añade tus canciones favoritas.</p>
+    <template v-if="$store.state.myQueue.length">
+      <router-link to="/queue">
+        <div class="app-QueueShortcut">
+          <p>Mi cola de reproducción actual</p>
+        </div>
+      </router-link>
+    </template>
+    <p v-else>Para crear una cola de reproducción, usa el buscador y añade tus canciones favoritas.</p>
   </div>
 </template>
 <script>
